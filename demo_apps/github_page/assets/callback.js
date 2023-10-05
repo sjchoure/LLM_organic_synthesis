849c5ab81781e4291eeb4260c183f49aec12c9fe
Sourabh Choure <sourabhchoure98@gmail.com>
2023-10-05 13:48:41 -0400

    Start of React App

--- demo_apps/github_page/assets/callback.js
+++
@@ -1,9 +0,0 @@
-if (!window.dash_clientside) {
-    window.dash_clientside = {}
-}
-
-window.dash_clientside.clientside = {
-    update_reaction: function(reaction_id, clean_data) {
-	    return clean_data[reaction_id]
-        }
-    }
